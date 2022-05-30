db5a3e68db60c623b565f41d17561d9862748f28
santocaridi99 <santocaridi99@gmail.com>
2022-05-30 17:56:28 +0200

    creazione file

--- la-mia-pizzeria-static/Controllers/HomeController.cs
+++ la-mia-pizzeria-static/Controllers/HomeController.cs
@@ -15,16 +15,6 @@ namespace la_mia_pizzeria_static.Controllers
 
         public IActionResult Index()
         {
-            //informazioni pizza
-            //List<string> nome = new List<string>() { "margherita", "capricciosa", "salsiccia e patate", "marinara", "4 stagioni" };
-            //List<string> descrizione = new List<string>() { "la classica pizza margherita", "la nostrà specialità", "la golosa", "grande classico", "la più venduta" };
-            ////inserire le immagini
-            //List<int> prezzo = new List<int>() { 3, 4, 5, 3, 4 };
-
-
-            //ViewData["nome"] = nome;
-            //ViewData["descrizione"] = descrizione;
-            //ViewData["prezzo"] = prezzo;
             return View();
         }
 
